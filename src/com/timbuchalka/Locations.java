dc591fba49e7709c149db03cf4b19daee786a175
brunorebic <bruno.rebic@gmail.com>
2022-12-06 12:12:28 +0100

    Reading and writing using Files class

--- src/com/timbuchalka/Locations.java
+++ src/com/timbuchalka/Locations.java
@@ -1,28 +1,48 @@
 package com.timbuchalka;
 
 import java.io.*;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.util.*;
 
 /**
  * Created by timbuchalka on 2/04/2016.
  */
 public class Locations implements Map<Integer, Location> {
-    private static Map<Integer, Location> locations = new LinkedHashMap<>();
+    private static final Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        //writing object to file using java.nio
-        Path locPath = FileSystems.getDefault().getPath("locations.dat");
 
-        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))) {
-            for (Location location : locations.values()) {
-                locFile.writeObject(location);
+        //writing to file only using java.nio
+        Path newFile = FileSystems.getDefault().getPath("locations_nio.txt");
+        StringBuilder builder = new StringBuilder();
+
+        for (Location location : locations.values()) {
+            builder.append(location.getLocationID());
+            builder.append(",");
+            builder.append(location.getDescription());
+            builder.append("\n");
+
+            for (String direction : location.getExits().keySet()) {
+                builder.append(direction);
+                builder.append(",");
+                builder.append(location.getExits().get(direction));
+                builder.append("\n");
             }
         }
+        //when writing using Files.write we are writing bytes to file
+        Files.write(newFile, builder.toString().getBytes(StandardCharsets.UTF_8));
 
-        //writing variable by variable to file using java.nio
+        //writing object to file using java.nio and java.io
+//        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+//
+//        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))) {
+//            for (Location location : locations.values()) {
+//                locFile.writeObject(location);
+//            }
+//        }
+
+        //writing variable by variable to file using java.nio and java.io
 //        Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
 //        Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
 //
@@ -41,26 +61,60 @@ public class Locations implements Map<Integer, Location> {
     }
 
     static {
-        //reading objects from a file using java.nio
-        Path locPath = FileSystems.getDefault().getPath("locations.dat");
 
-        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(locPath)))) {
-            boolean isEof = false;
-            while (!isEof) {
-                try {
-                    Location location = (Location) locFile.readObject();
-                    locations.put(location.getLocationID(), location);
-                } catch (EOFException e) {
-                    isEof = true;
-                }
+        //reading from file using only java.nio for directions.txt
+        Path locPath = FileSystems.getDefault().getPath("locations.txt");
+        Path dirPath = FileSystems.getDefault().getPath("directions.txt");
+
+        try {
+            Scanner locFile = new Scanner(Files.newBufferedReader(locPath));
+            List<String> directionsList = Files.readAllLines(dirPath);
+
+            locFile.useDelimiter(",");
+
+            while (locFile.hasNextLine()) {
+                int locID = locFile.nextInt();
+                locFile.skip(locFile.delimiter());
+                String description = locFile.nextLine();
+
+                locations.put(locID, new Location(locID, description, null));
+            }
+
+            for (String str : directionsList) {
+                String[] data = str.split(",");
+                int locID = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int destination = Integer.parseInt(data[2]);
+
+                locations.get(locID).addExit(direction, destination);
             }
 
 
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
         }
+
+
+        //reading objects from a file using java.nio and java.io
+//        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+//
+//        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(locPath)))) {
+//            boolean isEof = false;
+//            while (!isEof) {
+//                try {
+//                    Location location = (Location) locFile.readObject();
+//                    locations.put(location.getLocationID(), location);
+//                } catch (EOFException e) {
+//                    isEof = true;
+//                }
+//            }
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
 
         //reading variable by variable from file using java.nio
 //        Path locPath = FileSystems.getDefault().getPath("locations.txt");
